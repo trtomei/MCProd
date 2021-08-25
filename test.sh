6ca118f498efe5a8a466d4561be22c5f5283b46f
John Stupak <jstupak@cern.ch>
2021-08-25 12:28:02 -0500

    forgot a script

---
+++ test.sh
@@ -0,0 +1,19 @@
+if [[ `basename $PWD` != "MCProd" ]]; then echo "Execute from MCProd dir"; exit; fi
+
+source setup.sh
+
+if [[ ! -d run ]]; then mkdir run;
+else rm -rf run/*;
+fi
+cd run
+../makeGridPacks.sh
+cd ..
+
+if [[ ! -d test ]]; then mkdir test;
+else rm -rf test/*;
+fi
+cd test
+cp ../run/gridpacks/13TeV_BB.tar.gz .
+tar -xzvf 13TeV_BB.tar.gz
+
+./run.sh 10000 8
