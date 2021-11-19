70224c26e0f5453db814dc4904e4663ff415354d
John Stupak <jstupak@cern.ch>
2021-11-19 16:13:56 -0600

    working recipe

--- install.sh
+++ install.sh
@@ -66,7 +66,7 @@ if [[ $? -ne 0 ]]; then
 fi
 cd ..
 
-#Needed for rivet (and delphes?)
+#Needed for Rivet
 source /cvmfs/sft.cern.ch/lcg/releases/LCG_99/ROOT/v6.22.06/x86_64-centos7-gcc10-opt/ROOT-env.sh
 
 #Rivet
@@ -77,40 +77,3 @@ if [[ $? -ne 0 ]]; then
     echo "ERROR installing rivet"
     exit
 fi
-#source rivetenv.sh
-
-# #MadGraph
-# wget https://launchpad.net/mg5amcnlo/3.0/3.3.x/+download/MG5_aMC_v2.9.7.tar.gz
-# tar -xzvf MG5_aMC_v2.9.7.tar.gz
-# echo "install pythia8" | python ./MG5_aMC_v2_9_7/bin/mg5_aMC
-# if [[ $? -ne 0 ]]; then
-#     echo "ERROR getting madgraph"
-#     exit
-# fi
-# rm MG5_aMC_v2.9.7.tar.gz
-
-# #Pythia
-# # wget https://pythia.org/download/pythia83/pythia8306.tgz
-# # tar -xzvf pythia8306.tgz
-# # cd pythia8306
-# # ./configure --prefix=$prodBase --with-hepmc2=$prodBase
-# # make -j
-# # make install
-# # if [[ $? -ne 0 ]]; then
-# #     echo "ERROR compiling pythia"
-# #     exit
-# # fi
-# # rm pythia8306.tgz
-# # cd ..
-
-# #Delphes
-# #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${prodBase}/lib    
-# #export PYTHIA8=$prodBase
-# git clone https://github.com/delphes/delphes.git
-# cd delphes
-# make -j8 #HAS_PYTHIA8=true -I${prodBase}/include/Pythia8
-# if [[ $? -ne 0 ]]; then
-#     echo "ERROR compiling delphes"
-#     exit
-# fi
-# cd ..
