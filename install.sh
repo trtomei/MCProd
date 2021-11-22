f81cac4edac24e753180a7f611c75cb1c54f844c
John Stupak <jstupak@cern.ch>
2021-11-22 10:03:34 -0600

    using pythia from MG

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ INSTALL_PREFIX=$prodBase ./rivet-bootstrap
 #MadGraph
 wget https://launchpad.net/mg5amcnlo/3.0/3.1.x/+download/MG5_aMC_v3.1.1.tar.gz
 tar -xzvf MG5_aMC_v3.1.1.tar.gz
+echo "install pythia8" | python ./MG5_aMC_v3_1_1/bin/mg5_aMC
 if [[ $? -ne 0 ]]; then
     echo "ERROR getting madgraph"
     exit
@@ -17,28 +18,26 @@ fi
 rm MG5_aMC_v3.1.1.tar.gz
 
 #Pythia
-wget https://pythia.org/download/pythia83/pythia8306.tgz
-tar -xzvf pythia8306.tgz
-cd pythia8306
-./configure --prefix=$prodBase --with-hepmc2=$prodBase
-make -j
-make install
-if [[ $? -ne 0 ]]; then
-    echo "ERROR compiling pythia"
-    exit
-fi
-rm pythia8306.tgz
-cd ..
+# wget https://pythia.org/download/pythia83/pythia8306.tgz
+# tar -xzvf pythia8306.tgz
+# cd pythia8306
+# ./configure --prefix=$prodBase --with-hepmc2=$prodBase
+# make -j
+# make install
+# if [[ $? -ne 0 ]]; then
+#     echo "ERROR compiling pythia"
+#     exit
+# fi
+# rm pythia8306.tgz
+# cd ..
 
 #Delphes
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${prodBase}/lib    
 export PYTHIA8=$prodBase
 git clone https://github.com/delphes/delphes.git
 cd delphes
-make HAS_PYTHIA8=true -j -I${prodBase}/include/Pythia8
+make -j #HAS_PYTHIA8=true -I${prodBase}/include/Pythia8
 if [[ $? -ne 0 ]]; then
     echo "ERROR compiling delphes"
     exit
 fi
-
-#echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${prodBase}/lib" >> ~/.bash_profile
