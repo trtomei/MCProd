99f161769fe76ae046fe9850b66bffaf8fc6f9d6
John Stupak <jstupak@cern.ch>
2021-11-30 15:23:48 -0600

    trying python3

--- setup.sh
+++ setup.sh
@@ -2,19 +2,18 @@ if [[ `basename $PWD` != "MCProd" ]]; then echo "Execute from MCProd dir"; exit;
 
 export prodBase=$PWD
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${prodBase}/lib #:${prodBase}/MG5_aMC_v3_1_1/HEPTools/lhapdf6_py3/lib
-export PATH=/cvmfs/sft.cern.ch/lcg/external/texlive/2016/bin/x86_64-linux:$PATH
-export lhapdf=$prodBase/MG5_aMC_v3_1_1/HEPTools/bin/lhapdf-config
-
-#if [[ -e rivetenv.sh ]]; then source rivetenv.sh; fi
-
 if [[ $HOSTNAME == "login.snowmass21.io" ]]; then
+    module purge
     #important
-    module load python/2.7.15
-    module load py-numpy/1.15.2-py2.7
+
+    #module load python/2.7.15
+    #module load py-numpy/1.15.2-py2.7
+    module load python/3.7.0
+    module load py-numpy/1.15.2-py3.7
     #module load py-six/1.11.0-py2.7
-    #. /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.22.08/x86_64-centos7-gcc48-opt/bin/thisroot.sh
-    source /cvmfs/sft.cern.ch/lcg/releases/LCG_99/ROOT/v6.22.06/x86_64-centos7-gcc10-opt/ROOT-env.sh
+    #. /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.22.08/x86_64-centos7-gcc48-opt/bin/thisroot.sh  #original
+    source /cvmfs/sft.cern.ch/lcg/releases/LCG_99/ROOT/v6.22.06/x86_64-centos7-gcc10-opt/ROOT-env.sh  #for rivet
+    #. /cvmfs/sft.cern.ch/lcg/releases/LCG_99/ROOT/v6.22.08/x86_64-centos7-gcc48-opt/ROOT-env.sh
     
     #convenient
     module load emacs
@@ -25,3 +24,9 @@ else
 	exit
     fi
 fi
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${prodBase}/lib #:${prodBase}/MG5_aMC_v3_1_1/HEPTools/lhapdf6_py3/lib                             
+export PATH=$PATH:/cvmfs/sft.cern.ch/lcg/external/texlive/2016/bin/x86_64-linux
+export lhapdf=$prodBase/MG5_aMC_v3_1_1/HEPTools/bin/lhapdf-config
+
+#if [[ -e rivetenv.sh ]]; then source rivetenv.sh; fi
