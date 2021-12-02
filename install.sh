844d26fa90041e1039653db16066f5b479f66043
John Stupak <jstupak@cern.ch>
2021-12-01 18:19:47 -0600

    arrrrrgh

--- install.sh
+++ install.sh
@@ -8,6 +8,28 @@ if [[ `basename $PWD` != "MCProd" ]]; then echo "Execute from MCProd dir"; exit;
 #cmake -DCMAKE_INSTALL_PREFIX=$prodBase -DCMAKE_CXX_STANDARD=14 ../root-6.24.06/
 #cmake --build . --target install -- -j8
 
+#MadGraph                                                                                                                                  
+wget https://launchpad.net/mg5amcnlo/3.0/3.3.x/+download/MG5_aMC_v2.9.7.tar.gz
+tar -xzvf MG5_aMC_v2.9.7.tar.gz
+echo "install pythia8" | python ./MG5_aMC_v2_9_7/bin/mg5_aMC
+if [[ $? -ne 0 ]]; then
+    echo "ERROR getting madgraph"
+    exit
+fi
+rm MG5_aMC_v2.9.7.tar.gz
+
+#Delphes
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${prodBase}/lib
+#export PYTHIA8=$prodBase
+git clone https://github.com/delphes/delphes.git
+cd delphes
+make -j8 #HAS_PYTHIA8=true -I${prodBase}/include/Pythia8
+if [[ $? -ne 0 ]]; then
+    echo "ERROR compiling delphes"
+    exit
+fi
+cd ..
+
 #Rivet dependencies
 python3 -m pip install python-dev-tools --user --upgrade
 if [[ $? -ne 0 ]]; then
@@ -29,45 +51,46 @@ cd ..
 #Rivet
 wget https://gitlab.com/hepcedar/rivetbootstrap/raw/3.1.4/rivet-bootstrap
 chmod +x rivet-bootstrap
+source /cvmfs/sft.cern.ch/lcg/releases/LCG_99/ROOT/v6.22.06/x86_64-centos7-gcc10-opt/ROOT-env.sh
 INSTALL_PREFIX=$prodBase ./rivet-bootstrap
 if [[ $? -ne 0 ]]; then
     echo "ERROR installing rivet"
     exit
 fi
-source rivetenv.sh
+#source rivetenv.sh
 
-#MadGraph
-wget https://launchpad.net/mg5amcnlo/3.0/3.3.x/+download/MG5_aMC_v2.9.7.tar.gz
-tar -xzvf MG5_aMC_v2.9.7.tar.gz
-echo "install pythia8" | python ./MG5_aMC_v2_9_7/bin/mg5_aMC
-if [[ $? -ne 0 ]]; then
-    echo "ERROR getting madgraph"
-    exit
-fi
-rm MG5_aMC_v2.9.7.tar.gz
-
-#Pythia
-# wget https://pythia.org/download/pythia83/pythia8306.tgz
-# tar -xzvf pythia8306.tgz
-# cd pythia8306
-# ./configure --prefix=$prodBase --with-hepmc2=$prodBase
-# make -j
-# make install
+# #MadGraph
+# wget https://launchpad.net/mg5amcnlo/3.0/3.3.x/+download/MG5_aMC_v2.9.7.tar.gz
+# tar -xzvf MG5_aMC_v2.9.7.tar.gz
+# echo "install pythia8" | python ./MG5_aMC_v2_9_7/bin/mg5_aMC
 # if [[ $? -ne 0 ]]; then
-#     echo "ERROR compiling pythia"
+#     echo "ERROR getting madgraph"
 #     exit
 # fi
-# rm pythia8306.tgz
-# cd ..
+# rm MG5_aMC_v2.9.7.tar.gz
 
-#Delphes
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${prodBase}/lib    
-#export PYTHIA8=$prodBase
-git clone https://github.com/delphes/delphes.git
-cd delphes
-make -j8 #HAS_PYTHIA8=true -I${prodBase}/include/Pythia8
-if [[ $? -ne 0 ]]; then
-    echo "ERROR compiling delphes"
-    exit
-fi
-cd ..
+# #Pythia
+# # wget https://pythia.org/download/pythia83/pythia8306.tgz
+# # tar -xzvf pythia8306.tgz
+# # cd pythia8306
+# # ./configure --prefix=$prodBase --with-hepmc2=$prodBase
+# # make -j
+# # make install
+# # if [[ $? -ne 0 ]]; then
+# #     echo "ERROR compiling pythia"
+# #     exit
+# # fi
+# # rm pythia8306.tgz
+# # cd ..
+
+# #Delphes
+# #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${prodBase}/lib    
+# #export PYTHIA8=$prodBase
+# git clone https://github.com/delphes/delphes.git
+# cd delphes
+# make -j8 #HAS_PYTHIA8=true -I${prodBase}/include/Pythia8
+# if [[ $? -ne 0 ]]; then
+#     echo "ERROR compiling delphes"
+#     exit
+# fi
+# cd ..
